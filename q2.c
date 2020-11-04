f17e800024b5ebcf840c066617e3305f2d11b56e
Akmal <ahmad6akmal99@gmail.com>
2020-11-04 08:21:54 +0000

    Question 2.2

---
+++ q2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc **argv){
+
+	int pid; //process ID
+
+	switch (pid = fork()){
+
+	case 0; //a fork  returns 0 to the child
+		printf("I'm the child process: child pid=%d\n", getpid());
+	        break;
+
+	default: //a fork returns a pid to the parent
+		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
+		break;
+
+	case -1: //something went wrong
+		perror("fork");
+		exit(1);
+
+	}
+	exit(0);
+}
