fbe00442a7e58eb9dec5069dd9db5fa6695c1742
Akmal <ahmad6akmal99@gmail.com>
2020-11-07 01:50:36 +0000

    adding 2 lines of codes

--- q2.c
+++ q2.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
 int main(int argc, char **argv){
 
@@ -13,6 +14,7 @@ int main(int argc, char **argv){
 	        break;
 
 	default: //a fork returns a pid to the parent
+		wait(NULL);
 		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
 		break;
 
